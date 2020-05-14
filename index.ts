cc5442870f1195216d6bc340c14f8ae7d28cf3e2
Joe Duffy <joe@pulumi.com>
2020-05-14 16:28:57 -0700

    Add moar buckets

--- index.ts
+++ index.ts
@@ -3,7 +3,11 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const names = [];
+for (let i = 0; i < 3; i++) {
+    const bucket = new aws.s3.Bucket(`my-bucket-${i}`);
+    names.push(bucket.id);
+}
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Export the name of the buckets
+export const bucketNames = names;

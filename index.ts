19cdc912767298d73c20504f93b6af361493a4d4
Xiang Wang (Peter) <xwang1314@gmail.com>
2021-08-30 14:35:36 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,19 +2,19 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-const config = new pulumi.Config();
-const vpcTag1 = config.requireObject("xw-cluster-4.k8s.local");
-const vpcTag2 = config.requireObject("xw-cluster-3.k8s.local");
+#const config = new pulumi.Config();
+#const vpcTag1 = config.requireObject("xw-cluster-4.k8s.local");
+#const vpcTag2 = config.requireObject("xw-cluster-3.k8s.local");
 
 const firstVpc = aws.ec2.getVpc({
     tags: {
-        Name: vpcTag1,
+        Name: "xw-cluster-4.k8s.local",
     },
 });
 
 const secVpc = aws.ec2.getVpc({
     tags: {
-        Name: vpcTag2,
+        Name: "xw-cluster-3.k8s.local",
     },
 });
 

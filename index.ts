49995417f2af1d5a63dac6cb99a585b44ffa9c17
Xiang Wang (Peter) <xwang1314@gmail.com>
2021-08-30 13:48:00 -0500

    vpc 1

--- index.ts
+++ index.ts
@@ -2,12 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const names = [];
-for (let i = 0; i < 3; i++) {
-    const bucket = new aws.s3.Bucket(`my-bucket-${i}`);
-    names.push(bucket.id);
-}
-
-// Export the name of the buckets
-export const bucketNames = names;
+const vpc = new aws.ec2.Vpc("xw-pulumi-vpc", {
+    cidrBlock: "10.129.0.0/16",
+});

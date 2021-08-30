6df4b044f50902c6633af4ffcd266e2d6d09b153
Xiang Wang (Peter) <xwang1314@gmail.com>
2021-08-30 14:22:39 -0500

    vpc 4 fix

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+const config = new pulumi.Config();
 const vpcTag1 = config.requireObject("xw-cluster-4.k8s.local");
 const vpcTag2 = config.requireObject("xw-cluster-3.k8s.local");
 
@@ -17,5 +18,5 @@ const secVpc  = aws.ec2.getVpc({
     },
 });
 
-export firstVpcId = firstVpc.id
-export secVpcId = secVpc.id
+export const firstVpcId = firstVpc.id
+export const secVpcId = secVpc.id

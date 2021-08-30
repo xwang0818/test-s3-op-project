0d6568f13ac4c115d10a8de4130e34fa83b71930
Xiang Wang (Peter) <xwang1314@gmail.com>
2021-08-30 17:00:06 -0500

    connection

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import * as awsx from "@pulumi/awsx";
 * const vpcTag2 = config.requireObject("xw-cluster-3.k8s.local");
 */
 
+/** https://github.com/pulumi/pulumi-aws/blob/d26fdf80632ded25a926f9d4ed2f5e7234dc4cf8/sdk/nodejs/ec2/getVpc.ts */
 const firstVpc = aws.ec2.getVpc({
     tags: {
         Name: "xw-cluster-4.k8s.local",
@@ -20,6 +21,7 @@ const secVpc = aws.ec2.getVpc({
     },
 });
 
+/** https://github.com/pulumi/pulumi-aws/blob/d26fdf80632ded25a926f9d4ed2f5e7234dc4cf8/sdk/nodejs/ec2/getRouteTables.ts */
 const rtsFirst = aws.ec2.getRouteTables({
     tags: {
         KubernetesCluster: "xw-cluster-4.k8s.local",
@@ -32,8 +34,19 @@ const rtsSec = aws.ec2.getRouteTables({
     },
 });
 
+/** https://github.com/pulumi/pulumi-aws/blob/52989a7f8b5fced978aff841d067ae702eac13a2/sdk/nodejs/ec2/vpcPeeringConnection.ts */
+const vpcPeeringConnection = new aws.ec2.VpcPeeringConnection("vpcPeeringConnection", {
+    peerVpcId: secVpc.id,
+    vpcId: firstVpc.id,
+    autoAccept: true,
+    tags: {
+        Name: "VPC Peering between fistVpc and secVpc",
+    },
+});
+
 export const firstVpcId = firstVpc
 export const secVpcId = secVpc
 export const firstRTs = rtsFirst
 export const secRTs = rtsSec
+export const vpcPeeringConnection
 

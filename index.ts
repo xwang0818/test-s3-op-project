c1e1e53669fe6aed5d0cd0546c537b478d9d0613
Xiang Wang (Peter) <xwang1314@gmail.com>
2021-08-30 21:33:01 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-import {Promise} from 'es6-promise';
 
 /**
 * const config = new pulumi.Config();
@@ -16,15 +15,11 @@ const firstVpc = aws.ec2.getVpc({
     },
 });
 
-const rfirstVpc = Promise.resolve(firstVpc)
-
 const secVpc = aws.ec2.getVpc({
     tags: {
         Name: "xw-cluster-3.k8s.local",
     },
 });
-
-const rsecVpc = Promise.resolve(secVpc)
 
 /** https://github.com/pulumi/pulumi-aws/blob/d26fdf80632ded25a926f9d4ed2f5e7234dc4cf8/sdk/nodejs/ec2/getRouteTables.ts */
 const rtsFirst = aws.ec2.getRouteTables({
@@ -41,8 +36,8 @@ const rtsSec = aws.ec2.getRouteTables({
 
 /** https://github.com/pulumi/pulumi-aws/blob/52989a7f8b5fced978aff841d067ae702eac13a2/sdk/nodejs/ec2/vpcPeeringConnection.ts */
 const vpcPeeringConnection = new aws.ec2.VpcPeeringConnection("vpcPeeringConnection", {
-    peerVpcId: rsecVpc.id,
-    vpcId: rfirstVpc.id,
+    peerVpcId: secVpc.id,
+    vpcId: firstVpc.id,
     autoAccept: true,
     tags: {
         Name: "VPC Peering between fistVpc and secVpc",
